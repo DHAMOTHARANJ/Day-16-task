6ba64352cd4eb9474aadd8b1fc211025a0e41653
DHAMOTHARANJ <123227517+DHAMOTHARANJ@users.noreply.github.com>
2023-03-22 14:07:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const log = (a) => console.log(a)
+let timer = document.createElement('div')
+timer.style='padding-top:50px'
+timer.style= 'text-shadow: 12px -24px 30px black'
+
+
+document.body.append(timer)
+
+setTimeout(()=> {
+  timer.textContent='10'
+  setTimeout(()=>{
+    timer.textContent='9'
+    setTimeout(()=>{
+      timer.textContent='8'
+      setTimeout(()=>{
+        timer.textContent='7'
+        setTimeout(()=>{
+          timer.textContent='6'
+          setTimeout(()=>{
+            timer.textContent='5'
+            setTimeout(()=>{
+              timer.textContent='4'
+              setTimeout(()=>{
+                timer.textContent='3'
+                setTimeout(()=>{
+                  timer.textContent='2'
+                  setTimeout(()=>{
+                    timer.textContent='1'
+                    setTimeout(()=>{
+                      timer.textContent='Happy Independance Day to All. The glory of Independence Day be with us forever !!!'
+                    
+                    })
+                  },1000)
+                },1000)
+              },1000)
+            },1000)
+          },1000)
+        },1000)
+      },1000)
+    },1000)
+  },1000)
+},1000)
